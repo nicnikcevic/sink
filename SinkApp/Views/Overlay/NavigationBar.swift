41fceddc04860e11a9df9b11991f5984892c5b33
Andrew Heller <andrewheller@Andrews-MacBook-Air-3.local>
2025-01-11 11:54:24 -0500

    File Formatting

---
+++ SinkApp/Views/Overlay/NavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBar.swift
+//  SinkApp
+//
+//  Created by Andrew Heller on 1/11/25.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
